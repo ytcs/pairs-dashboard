66d9a39eb0de07fc70d2d83c568e9e7de4533948
To Chin Yu <stevenytc@gmail.com>
2023-05-25 23:10:16 -0700

    change defaults

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+c1, c2, c3 = st.columns(3)
+with c1:
+    look_back = st.text_input('Look back (Days)', '600')
+with c2:
+    ticker1 = st.text_input('Long Ticker', 'AMD') # long
+with c3:
+    ticker2 = st.text_input('Short Ticker', 'INTC')  # short
 
-look_back = st.text_input('Look back (Days)', '90')
-ticker1 = st.text_input('Long Ticker', 'REZ') # long
-ticker2 = st.text_input('Short Ticker', 'NETL')  # short
+c4,c5 = st.columns([1,3])
+with c4:
+    fixed_weight = st.checkbox('Fixed Weights')
+with c5:
+    long_weight = st.text_input('Long Weight', value='0.5', disabled=not fixed_weight)
+    long_weight = float(long_weight)
 
 keep_cols = ['Date','Price','PctChange','PctVolatility']
 
@@ -27,7 +37,10 @@ s2 = s2[keep_cols]
 
 df =s1.set_index('Date').join(s2.set_index('Date'),lsuffix='1',rsuffix='2')
 
-pair_returns=df.eval('1+ (PctVolatility2 * PctChange1 - PctVolatility1* PctChange2)/ (PctVolatility1+PctVolatility2)').cumprod().rename('Pair Returns')-1
+if not fixed_weight:
+    pair_returns=df.eval('1+ (PctVolatility2 * PctChange1 - PctVolatility1* PctChange2)/ (PctVolatility1+PctVolatility2)').cumprod().rename('Pair Returns')-1
+else:
+    pair_returns=df.eval('1+ (@long_weight * PctChange1 - (1-@long_weight)* PctChange2)').cumprod().rename('Pair Returns')-1
 t1_returns = df.eval('1+PctChange1').cumprod().rename(f'{ticker1} Returns')-1
 t2_returns = df.eval('1+PctChange2').cumprod().rename(f'{ticker2} Returns')-1
 returns_df = pd.concat([pair_returns,t1_returns,t2_returns],axis=1)
@@ -35,8 +48,9 @@ returns_df = pd.concat([pair_returns,t1_returns,t2_returns],axis=1)
 fig1=px.line(returns_df,title=f'Pair (+{ticker1} -{ticker2})')
 st.plotly_chart(fig1)
 
-t1_weights = df.eval('PctVolatility2 / (PctVolatility1+PctVolatility2)').rename(f'{ticker1} Weight')
-t2_weights = df.eval('PctVolatility1 / (PctVolatility1+PctVolatility2)').rename(f'{ticker2} Weight')
-weights_df = pd.concat([t1_weights,t2_weights],axis=1)
-fig2=px.line(weights_df,title=f'Dollar Weights (+{ticker1} -{ticker2})')
-st.plotly_chart(fig2)
+if not fixed_weight:
+    t1_weights = df.eval('PctVolatility2 / (PctVolatility1+PctVolatility2)').rename(f'{ticker1} Weight')
+    t2_weights = df.eval('PctVolatility1 / (PctVolatility1+PctVolatility2)').rename(f'{ticker2} Weight')
+    weights_df = pd.concat([t1_weights,t2_weights],axis=1)
+    fig2=px.line(weights_df,title=f'Dollar Weights (+{ticker1} -{ticker2})')
+    st.plotly_chart(fig2)

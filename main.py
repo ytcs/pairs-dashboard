cd8fc293cda730d1f98076eb7fb7e1c395ac8889
To Chin Yu <stevenytc@gmail.com>
2023-05-29 14:13:39 -0700

    fixed rebalance logic

--- main.py
+++ main.py
@@ -1,46 +1,81 @@
 # %%
 from wallstreet import Stock
 import streamlit as st
+import numpy as np
 import pandas as pd
 import plotly.express as px
 
-c1, c2, c3 = st.columns(3)
+from datetime import date, timedelta
+
+# ===== UI =====
+c1, c2, c3 = st.columns([1,1,2])
 with c1:
-    look_back = st.text_input('Look back (Days)', '600')
+    start_date = st.date_input('Start Date', date.today()-timedelta(days=360))
 with c2:
-    ticker1 = st.text_input('Long Ticker', 'AMD') # long
+    end_date = st.date_input('End Date')
 with c3:
-    ticker2 = st.text_input('Short Ticker', 'INTC')  # short
+    tickers = st.text_input('Pair (separate by comma)', 'WM,SRCL') 
+    ticker1,ticker2 = [t.strip() for t in tickers.split(',')[:2]]
 
-c4,c5 = st.columns([1,3])
+c4,c5,c6 = st.columns([1,1,1])
 with c4:
-    fixed_weight = st.checkbox('Fixed Weights')
+    iv_weight = st.checkbox('IV Weights')
 with c5:
-    long_weight = st.text_input('Long Weight', value='0.5', disabled=not fixed_weight)
-    long_weight = float(long_weight)
+    long_weight = st.text_input('Long Weight', value='0.5', disabled=iv_weight)
+    long_weight = float(long_weight) 
+with c6:
+    rebalance_freq = st.text_input('Rebalance Frequency (Business Days)', value='60')
+    rebalance_freq = int(rebalance_freq)
+# ===========
 
-keep_cols = ['Date','Price','PctChange','PctVolatility']
+# ===== Grab Data =====
+keep_cols = ['Price','PctChange','PctVolatility','Position','Dollars']
+look_back = int((date.today()-start_date).days)
 
-s1 = Stock(ticker1).historical(int(look_back))
-s1['Price'] = s1['Adj Close']
-s1['PrevPrice'] = s1.Price.shift(1)
-s1['PctVolatility'] = s1.eval('(High-Low)/Price').shift(1)
-s1['PctChange'] = s1.eval('(Price-PrevPrice)/PrevPrice')
-s1 = s1[keep_cols]
+dfs = []
+fixed_weight = long_weight*2
+for t in tickers.split(',')[:2]:
+    s = Stock(t.strip()).historical(look_back)
+    s['Date'] = pd.to_datetime(s['Date'])
+    s.set_index('Date',inplace=True)
+    s['Price'] = s['Adj Close']
+    s['PrevPrice'] = s.Price.shift(1)
+    s['PctVolatility'] = s.eval('(High-Low)/Price').shift(1).rolling(rebalance_freq, min_periods=1).mean()
+    s['PctChange'] = s.eval('(Price-PrevPrice)/PrevPrice')
+    s.dropna(inplace=True)
+    s['Position'] = np.nan
+    s['Dollars'] = 1 if iv_weight else fixed_weight
+    fixed_weight = 2-fixed_weight
+    s = s[keep_cols]
+    dfs.append(s)
 
-s2 = Stock(ticker2).historical(int(look_back))
-s2['Price'] = s2['Adj Close']
-s2['PrevPrice'] = s2.Price.shift(1)
-s2['PctVolatility'] = s2.eval('(High-Low)/Price').shift(1)
-s2['PctChange'] = s2.eval('(Price-PrevPrice)/PrevPrice')
-s2 = s2[keep_cols]
+df = dfs[0].join(dfs[-1],lsuffix='1',rsuffix='2')[start_date:end_date]
+# ==========
 
-df =s1.set_index('Date').join(s2.set_index('Date'),lsuffix='1',rsuffix='2')
+# ===== Rebalance Logic =====
 
-if not fixed_weight:
-    pair_returns=df.eval('1+ (PctVolatility2 * PctChange1 - PctVolatility1* PctChange2)/ (PctVolatility1+PctVolatility2)').cumprod().rename('Pair Returns')-1
-else:
-    pair_returns=df.eval('1+ (@long_weight * PctChange1 - (1-@long_weight)* PctChange2)').cumprod().rename('Pair Returns')-1
+rebalance_dates = pd.date_range(df.index.min(),df.index.max(),freq=f'{rebalance_freq}B',inclusive='left')
+
+for i, d in enumerate(rebalance_dates):
+    next_d = rebalance_dates[i+1] if i < len(rebalance_dates)-1 else df.index.max()
+
+    if iv_weight:
+        long_weight = df.eval('PctVolatility2/(PctVolatility1+PctVolatility2)').loc[d]
+    total_dollars = df.loc[d].Dollars1+df.loc[d].Dollars2
+    df.loc[d:next_d,'EntryPrice1'] = df.loc[d].Price1
+    df.loc[d:next_d,'EntryPrice2'] = df.loc[d].Price2
+    df.loc[d:next_d,'Position1'] = long_weight*total_dollars/df.loc[d].Price1
+    df.loc[d:next_d,'Position2'] = (1-long_weight)*total_dollars/df.loc[d].Price2
+
+    df.loc[d:next_d,'Dollars1'] = df.loc[d:next_d].eval('Position1*(Price1-EntryPrice1)+Position1*EntryPrice1')
+    df.loc[d:next_d,'Dollars2'] = df.loc[d:next_d].eval('Position2*(EntryPrice2-Price2)+Position2*EntryPrice2')
+
+# st.dataframe(df)
+# ==========
+
+# ===== Plots =====
+
+pair_returns=df.eval('(Dollars1 + Dollars2)/2').rename('Pair Returns')-1
 t1_returns = df.eval('1+PctChange1').cumprod().rename(f'{ticker1} Returns')-1
 t2_returns = df.eval('1+PctChange2').cumprod().rename(f'{ticker2} Returns')-1
 returns_df = pd.concat([pair_returns,t1_returns,t2_returns],axis=1)
@@ -48,9 +83,13 @@ returns_df = pd.concat([pair_returns,t1_returns,t2_returns],axis=1)
 fig1=px.line(returns_df,title=f'Cumulative Returns (+{ticker1} -{ticker2})')
 st.plotly_chart(fig1)
 
-if not fixed_weight:
-    t1_weights = df.eval('PctVolatility2 / (PctVolatility1+PctVolatility2)').rename(f'{ticker1} Weight')
-    t2_weights = df.eval('PctVolatility1 / (PctVolatility1+PctVolatility2)').rename(f'{ticker2} Weight')
-    weights_df = pd.concat([t1_weights,t2_weights],axis=1)
-    fig2=px.line(weights_df,title=f'Dollar Weights (+{ticker1} -{ticker2})')
-    st.plotly_chart(fig2)
+pos_df = df[['Position1','Position2']]*1_000
+pos_df.columns = [ticker1,ticker2]
+fig2=px.line(pos_df,title=f'Position Size (Number of shares / $1k capital) (+{ticker1} -{ticker2})')
+st.plotly_chart(fig2)
+# if not fixed_weight:
+#     t1_weights = df.eval('PctVolatility2 / (PctVolatility1+PctVolatility2)').rename(f'{ticker1} Weight')
+#     t2_weights = df.eval('PctVolatility1 / (PctVolatility1+PctVolatility2)').rename(f'{ticker2} Weight')
+#     weights_df = pd.concat([t1_weights,t2_weights],axis=1)
+
+# ==========
